426033d01e8084c0600710159050087c33308286
PrismaticPolygon <domwhite97@gmail.com>
2020-03-14 21:11:29 +0000

    14-03-2020 model test 1.

--- models/test.py
+++ models/test.py
@@ -5,17 +5,38 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, BayesianRidge, SGDClassifier
 from joblib import dump, load   # More efficient than pickle for objects with large internal NumPy arrays
 
+# Let's do the rest of our stuff for parameters, based on best guesses....
+
+# Standardisation of datasets is a common requirement for many machine learning estimators implemented in scikit-learn.
+# Individual features should be Gaussian with zero mean and unit variance.
+# Given that they do work...
 
 def train():
 
     models = [
-        LogisticRegression(solver="lbfgs", random_state=0, max_iter=2000),
-        RandomForestClassifier(random_state=0, n_estimators=20),
+        LogisticRegression(),
+        BayesianRidge(),
+        SGDClassifier(
+            loss="hinge",       # (soft-margin) linear SVM
+            penalty="l2",
+            max_iter=5,
+            random_state=0
+        ),
+        RandomForestClassifier(
+            n_jobs=-1,              # Use all available cores on the machine,
+            n_estimators=20         # Number of trees. Default of 100 takes too long.
+        ),
+        GradientBoostingClassifier(
+            n_estimators=100,
+            learning_rate=1.0,      # Controls over-fitting via shrinkage
+            max_depth=1,            # Tree depth
+            random_state=0
+        )
     ]
 
     # https://stackoverflow.com/questions/4008546/how-to-pad-with-n-characters-in-python
@@ -42,69 +63,34 @@ def train():
             dump(model, file)
 
 
-def regression_importance(regressor: LogisticRegression):
-    """
-    Calculate the importance of features for logistic regression. From
+def graph(model, c, std=None, print_ranking=False):
 
-    https://stackoverflow.com/questions/47303261/getting-weights-of-features-using-scikit-learn-logistic-regression
-    https://towardsdatascience.com/model-based-feature-importance-d4f6fb2ad403
+    c = np.abs(c)
+    indices = np.argsort(c)[::-1]               # The indices that would sort c in descending order
+    num_features = X.shape[1]                   # The number of features
+    columns = [df.columns[i] for i in indices]
 
-    :param regressor: a classifier of type LogisticRegression
-    :return: None
-    """
+    print("Accuracy: {:.2f}".format(model.score(X_test, Y_test)))
 
-    c = regressor.coef_[0]          # Coefficients. A 1-D array.
-    indices = np.argsort(c)         # The indices that would sort c in ascending order. Lower is more important
-    columns = [df.columns[i] for i in indices]  # That is not right.
+    if print_ranking:
 
-    for i in range(X.shape[1]):
+        for i in range(num_features):
 
-        print("{}. {} ({:.4E})".format(i + 1, df.columns[indices[i]], c[indices[i]]))
+            print("{}. {} ({:.4E})".format(i + 1, df.columns[indices[i]], c[indices[i]]))
 
     plt.figure()
-    plt.title("Logistic regression importances")
-    plt.bar(range(X.shape[1]), c[indices], color="b", align="center")
-    plt.xticks(range(X.shape[1]), columns, rotation=90)
-    plt.xlim([-1, X.shape[1]])
-    plt.show()
+    plt.title("{} importance".format(model.__class__.__name__))
 
+    if std is not None:
 
-def forest_importance(forest: RandomForestClassifier):
+        plt.bar(range(num_features), c[indices], color="r", yerr=std[indices], align="center")
 
-    """
-    Calculate the importance of features for the random forest.
+    else:
 
-    https://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html
+        plt.bar(range(num_features), c[indices], color="r", align="center")
 
-    :param forest: a classifier of type RandomForestClassifier
-    :return: None
-    """
-
-    importances = forest.feature_importances_
-
-    std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
-    indices = np.argsort(importances)[::-1]
-    columns = [df.columns[i] for i in indices]  # That is not right.
-
-    # I can't wrap my head around what is!
-
-    # Print the feature ranking
-    print("Feature ranking:")
-
-    # We should be able to sync that up with column names as well.
-    # THe indices is just a sorted ist of importance indices. Gotcha.
-    # I.e. the most important feature has an index of indices[i]
-    # Okay. Let's figure out the logistic regression.
-
-    for i in range(X.shape[1]):
-
-        print("{}. {} ({:.4f})".format(i + 1, df.columns[indices[i]], importances[indices[i]]))
-
-    plt.figure()
-    plt.title("Random forest feature importances")
-    plt.bar(range(X.shape[1]), importances[indices], color="r", yerr=std[indices], align="center")
-    plt.xticks(range(X.shape[1]), columns, rotation=90)
-    plt.xlim([-1, X.shape[1]])
+    plt.xticks(range(num_features), columns, rotation=90)
+    plt.xlim([-1, num_features])
     plt.show()
 
 
@@ -116,24 +102,36 @@ def test():
 
         if extension == "pickle":
 
-            print("Loading {}...".format(file), end="")
+            print("\nLoading {}...".format(file), end="")
 
             start = time.time()
 
             path = os.path.join("models", file)
             model = load(path)
 
-            print(" DONE ({:.2f}s)".format(time.time() - start))
+            print(" DONE ({:.2f}s)\n".format(time.time() - start))
 
-            if name == "RandomForestClassifier":
+            if isinstance(model, RandomForestClassifier):
 
-                forest_importance(model)
+                std = np.std([tree.feature_importances_ for tree in model.estimators_], axis=0)
+                graph(model, model.feature_importances_, std=std)
 
-            if name == "LogisticRegression":
+            elif isinstance(model, GradientBoostingClassifier):
 
-                regression_importance(model)
+                graph(model, model.feature_importances_)
 
-# Possible that they aren't saved in my pickle. I hope that they are though.
+            elif isinstance(model, LogisticRegression):
+
+                graph(model, model.coef_[0])
+
+            elif isinstance(model, BayesianRidge):
+
+                graph(model, model.coef_)
+
+            elif isinstance(model, SGDClassifier):
+
+                graph(model, model.coef_[0])
+
 
 if __name__ == "__main__":
 
@@ -150,4 +148,5 @@ if __name__ == "__main__":
 
     print("DONE ({:.2f}s)".format(time.time() - start))
 
+    # train()
     test()
